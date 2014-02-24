f13c4baef869dcf5f1ed925638c6721e396b2ab5
frissan90 <fredriklindgren90@gmail.com>
2014-02-24 10:55:23 +0100

    Kom igen

---
+++ Phonetoys/PhonetoysWeb/Register.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PhonetoysWeb
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            countryDD.Items.Add("Select Country");
+            countryDD.Items.Add("Sverige");
+            countryDD.Items.Add("Norge");
+            countryDD.Items.Add("Danmark");
+            countryDD.Items.Add("Suomi");
+        }
+    }
+}
---
+++ Phonetoys/PhonetoysWeb/test.aspx
@@ -0,0 +1,7 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Phonetoys.Master" AutoEventWireup="true" CodeBehind="test.aspx.cs" Inherits="PhonetoysWeb.test" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="topContent" runat="server">
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="MainContent" runat="server">
+</asp:Content>
