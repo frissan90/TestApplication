74703c03e539067863f3ec72c625e0d80438fc7d
frissan90 <fredriklindgren90@gmail.com>
2014-02-26 12:41:39 +0100

    släck fungerar kanske

--- PhoneToys/PhoneToys/Light.aspx.cs
+++ PhoneToys/PhoneToys/Light.aspx.cs
@@ -18,7 +18,10 @@ namespace PhoneToys
 
         protected void LightOffBTN_Click(object sender, EventArgs e)
         {
-            
+            var client = new RestClient("https://api.spark.io/v1/devices/48ff6c065067555035261587/LightOff?access_token=0db5f14f8deff81a1ac03b43508f15e7e8637af7");
+            var request = new RestRequest(Method.POST);
+            request.RequestFormat = DataFormat.Json;
+            IRestResponse response = client.Execute(request);
         }
 
         protected void LightOnBTN_Click(object sender, EventArgs e)
