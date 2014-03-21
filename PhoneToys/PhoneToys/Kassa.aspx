02d8095c8c9954f13d17e4f57fdd275e79fdfcac
frissan90 <fredriklindgren90@gmail.com>
2014-03-21 11:30:50 +0100

    wfewfw

--- PhoneToys/PhoneToys/Kassa.aspx
+++ PhoneToys/PhoneToys/Kassa.aspx
@@ -4,4 +4,7 @@
 <asp:Content ID="Content2" ContentPlaceHolderID="FeaturedContent" runat="server">
 </asp:Content>
 <asp:Content ID="Content3" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="row">
+        <asp:Button runat="server" CssClass="button radius" ID="CheckOutBTN" Text="Bekräfta köp" OnClick="CheckOutBTN_Click" />
+    </div>
 </asp:Content>
--- PhoneToys/PhoneToys/Kassa.aspx.cs
+++ PhoneToys/PhoneToys/Kassa.aspx.cs
@@ -4,14 +4,23 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Entities;
+using DataAccessLayer;
 
 namespace PhoneToys
 {
     public partial class Kassa : System.Web.UI.Page
     {
+
+        private Data data = new Data();
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void CheckOutBTN_Click(object sender, EventArgs e)
+        {
+            data.kopSagor(Session["Username"].ToString(), (List<Varukorgen>)Session["varukorg"]);
         }
     }
 }
